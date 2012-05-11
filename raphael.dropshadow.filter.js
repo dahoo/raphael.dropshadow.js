b7a280b4d9da34b9ef1280d9febd7bd3e81d886f
Garth Braithwaite <garthdb@gmail.com>
2012-05-11 15:01:55 -0700

    Added support for opacity with feColorMatrix

--- raphael.dropshadow.filter.js
+++ raphael.dropshadow.filter.js
@@ -20,10 +20,11 @@
                 return document.createElementNS("http://www.w3.org/2000/svg", el);
             }
         };
-        Raphael.fn.addDropShadowFilter = function (size, offsetX, offsetY) {
+        Raphael.fn.addDropShadowFilter = function (size, offsetX, offsetY, opacity) {
             if (size != "none") {
                 var fltr = $("filter"),
                     blur = $("feGaussianBlur"),
+                    colorMatrix = $("feColorMatrix"),
                     offset = $("feOffset"),
                     merge = $("feMerge"),
                     mergeNodeShadow = $("feMergeNode"),
@@ -32,10 +33,12 @@
                 $(fltr, {height: "130%", width: "130%"});
                 $(blur, {stdDeviation: +size});
                 $(blur, {in: "SourceAlpha"});
+                $(colorMatrix, {result: "bluralpha", type: "bluralpha", values: "1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 "+opacity+" 0 "});
                 $(offset, {dx: offsetX, dy: offsetY, result: "offsetblur"});
                 $(mergeNodeShadow, {in: "offsetblur"});
                 $(mergeNodeSource, {in: "SourceGraphic"});
                 fltr.appendChild(blur);
+                fltr.appendChild(colorMatrix);
                 fltr.appendChild(offset);
                 fltr.appendChild(merge);
                 merge.appendChild(mergeNodeShadow);
